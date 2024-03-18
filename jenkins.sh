0e856c13320bf2e9b8dff862d75bafb17a7602fb
Anusha <ubuntu@ip-172-31-18-202.ec2.internal>
2024-03-18 04:15:49 +0000

    first commit

---
+++ jenkins.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+sudo apt update
+sudo apt -y upgrade
+sudo apt -y install openjdk-11-jdk
+sudo apt -y install maven
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins -y
+
